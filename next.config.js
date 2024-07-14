49b08f2d0f458ae00f62cb679bdbf6fc008b1b1a
Garrett O'Grady <garrettogrady@Garretts-MacBook-Air.local>
2024-07-14 10:02:57 -0700

    diables build errors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {};
 module.exports = nextConfig;
 
 module.exports = {
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
     images: {
         formats: ["image/avif", "image/webp"],
         remotePatterns: [
