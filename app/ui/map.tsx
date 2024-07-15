ce216274ddb417782bd2b9279e44a91dce5eb0fd
Garrett O'Grady <garrettogrady@Garretts-MacBook-Air.local>
2024-07-14 21:59:35 -0700

    adds map

--- app/ui/map.tsx
+++ app/ui/map.tsx
@@ -1,19 +1,20 @@
-'use client'
+'use client';
 
 import React, { useEffect, useRef, useState } from 'react';
 import { useJsApiLoader } from "@react-google-maps/api";
-import {Promotion, PromotionGrid} from "@/app/lib/definitions";
+import { PromotionGrid } from "@/app/lib/definitions";
 
 interface MapOptions {
     center: google.maps.LatLngLiteral;
     zoom: number;
     mapId: string;
 }
+
 interface MapProps {
     promotions: PromotionGrid[];
 }
 
-function Map({promotions}: MapProps) {
+function Map({ promotions }: MapProps) {
     const [map, setMap] = useState<google.maps.Map | null>(null);
     const mapRef = useRef<HTMLDivElement>(null);
     const [showMap, setShowMap] = useState(true);
@@ -43,60 +44,65 @@ function Map({promotions}: MapProps) {
                 mapId: '1234map'
             };
 
-            // Check if google object and Map are accessible
             if (google && google.maps && google.maps.Map) {
                 const gMap = new google.maps.Map(mapRef.current as HTMLDivElement, mapOptions);
-                console.log('gMap:', gMap);
-
                 setMap(gMap);
-                console.log(promotions)
-                promotions.map((promotion) => {
-                    const myLatLng = { lat: Number(promotion.locationLat) , lng: Number(promotion.locationLng) };
-                    new google.maps.marker.AdvancedMarkerElement({
-                        position: myLatLng,
-                        map,
-                        title: promotion.title,
-                    });
-                })
             } else {
                 console.error('Google Maps API not loaded correctly');
             }
         }
     }, [isLoaded]);
 
+    useEffect(() => {
+        if (isLoaded && map && promotions.length > 0) {
+            addMarkers(promotions);
+        }
+    }, [isLoaded, map, promotions]);
+
+    const setMarker = (location: google.maps.LatLng, name: string) => {
+        if (!map) return;
+
+        const marker = new google.maps.Marker({
+            map,
+            position: location,
+            title: name,
+        });
+
+        const infoWindow = new google.maps.InfoWindow({
+            content: buildMapInfoCardContent(name, ""),
+        });
+
+        marker.addListener("click", () => {
+            infoWindow.open(map, marker);
+        });
+
+        infoWindow.open(map, marker);
+    };
+
+    const addMarkers = (promotions: PromotionGrid[]) => {
+        if (!map) return;
+
+        promotions.forEach((promotion) => {
+            const location = new google.maps.LatLng({
+                lat: Number(promotion.locationLat),
+                lng: Number(promotion.locationLng),
+            });
+
+            setMarker(location, promotion.title);
+        });
+    };
+
     if (loadError) {
         return <div>Error loading maps</div>;
     }
 
-    function setMarker(location: google.maps.LatLng, name: string) {
-        if (!map) return;
-
-        map.setCenter(location)
-        const marker = new google.maps.marker.AdvancedMarkerElement({
-            map: map,
-            position: location,
-            title: "marker"
-
-        })
-        const infoCard = new google.maps.InfoWindow({
-            position: location,
-            content: buildMapInfoCardContent(name, name),
-            maxWidth: 200
-        })
-        infoCard.open({
-            map: map,
-            anchor: marker
-        })
-    }
-
-    // @ts-ignore
-    // @ts-ignore
     return (
         <div>
-            { isLoaded && showMap ?
+            {isLoaded && showMap ? (
                 <div style={{ height: '600px' }} ref={mapRef} />
-                : <p>Loading....</p>
-            }
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
     );
 }
