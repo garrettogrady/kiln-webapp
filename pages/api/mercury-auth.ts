4cc0f08579d398c0d20b56ea7f0427f5251c41e5
Garrett O'Grady <Garrettogrady@gmail.com>
2024-10-10 09:18:52 -0400

    Update mercury-auth.ts

--- pages/api/mercury-auth.ts
+++ pages/api/mercury-auth.ts
@@ -6,7 +6,9 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
-        return res.status(405).json({ message: 'Method Not Allowed' });
+        console.log("Method Not Allowed");
+        console.log("HI");
+        return res.status(405).json({ message: 'Method Not Allowed bruh' });
     }
 
     const { cardSuffix, factorToken } = req.body;
